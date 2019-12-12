a5528fd88859312d6d37ef6a40a06745a8865144
Edward Beech <initialed85@gmail.com>
2019-12-12 21:04:44 +0800

    Some tweaks

--- docker_run_in_background.sh
+++ docker_run_in_background.sh
@@ -11,5 +11,5 @@ docker run \
   -p 8081:8081 \
   -p 8082:8082 \
   -v "$(pwd)"/configs:/etc/motion \
-  -v /media/storage/Cameras/events:/srv/target_dir \
+  -v /media/storage/Cameras:/srv/target_dir \
   "${IMAGE_NAME}"
