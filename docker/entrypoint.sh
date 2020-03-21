5b70478de2dffbf1ba1f0ad7ec3ca466fbc63d0e
Edward Beech <initialed85@gmail.com>
2020-03-21 19:57:51 +0800

    Fix thing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,17 +10,19 @@ set -e
 
 EVENT_STORE=/srv/target_dir/events/persistence.jsonl
 EVENT_STORE_BACKUP=${EVENT_STORE}.old
-SEGMENT_STORE=/srv/target_dir/events/persistence.jsonl
+SEGMENT_STORE=/srv/target_dir/segments/persistence.jsonl
 SEGMENT_STORE_BACKUP=${SEGMENT_STORE}.old
 
 echo "backing up events store and clearing original"
 cp -frv ${EVENT_STORE} ${EVENT_STORE_BACKUP}
-echo "" > ${EVENT_STORE}
+cp -frv ${EVENT_STORE} "${EVENT_STORE_BACKUP}_$(date)"
+echo "" >${EVENT_STORE}
 echo ""
 
 echo "backing up segments store and clearing original"
 cp -frv ${SEGMENT_STORE} ${SEGMENT_STORE_BACKUP}
-echo "" > ${SEGMENT_STORE}
+cp -frv ${SEGMENT_STORE} "${SEGMENT_STORE_BACKUP}_$(date)"
+echo "" >${SEGMENT_STORE}
 echo ""
 
 echo "deduplicating events store"
