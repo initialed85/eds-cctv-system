6d901dd8d0134abfb49669172c82d906d121414f
Edward Beech <initialed85@gmail.com>
2020-04-10 03:01:05 +0800

    Reenable deduplicator

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,30 +8,30 @@
 
 set -e
 
-#EVENT_STORE=/srv/target_dir/events/persistence.jsonl
-#EVENT_STORE_BACKUP=${EVENT_STORE}.old
-#SEGMENT_STORE=/srv/target_dir/segments/persistence.jsonl
-#SEGMENT_STORE_BACKUP=${SEGMENT_STORE}.old
+EVENT_STORE=/srv/target_dir/events/persistence.jsonl
+EVENT_STORE_BACKUP=${EVENT_STORE}.old
+SEGMENT_STORE=/srv/target_dir/segments/persistence.jsonl
+SEGMENT_STORE_BACKUP=${SEGMENT_STORE}.old
 
-#echo "backing up events store and clearing original"
-#cp -frv ${EVENT_STORE} ${EVENT_STORE_BACKUP}
-#cp -frv ${EVENT_STORE} "${EVENT_STORE_BACKUP}_$(date)"
-#echo "" >${EVENT_STORE}
-#echo ""
+echo "backing up events store and clearing original"
+cp -frv ${EVENT_STORE} ${EVENT_STORE_BACKUP}
+cp -frv ${EVENT_STORE} "${EVENT_STORE_BACKUP}_$(date)"
+echo "" >${EVENT_STORE}
+echo ""
 
-#echo "backing up segments store and clearing original"
-#cp -frv ${SEGMENT_STORE} ${SEGMENT_STORE_BACKUP}
-#cp -frv ${SEGMENT_STORE} "${SEGMENT_STORE_BACKUP}_$(date)"
-#echo "" >${SEGMENT_STORE}
-#echo ""
+echo "backing up segments store and clearing original"
+cp -frv ${SEGMENT_STORE} ${SEGMENT_STORE_BACKUP}
+cp -frv ${SEGMENT_STORE} "${SEGMENT_STORE_BACKUP}_$(date)"
+echo "" >${SEGMENT_STORE}
+echo ""
 
-#echo "deduplicating events store"
-#event_store_deduplicator -sourcePath ${EVENT_STORE_BACKUP} -destinationPath ${EVENT_STORE}
-#echo ""
+echo "deduplicating events store"
+event_store_deduplicator -sourcePath ${EVENT_STORE_BACKUP} -destinationPath ${EVENT_STORE}
+echo ""
 
-#echo "deduplicating segments store"
-#event_store_deduplicator -sourcePath ${SEGMENT_STORE_BACKUP} -destinationPath ${SEGMENT_STORE}
-#echo ""
+echo "deduplicating segments store"
+event_store_deduplicator -sourcePath ${SEGMENT_STORE_BACKUP} -destinationPath ${SEGMENT_STORE}
+echo ""
 
 echo "starting supervisord"
 supervisord -n -c /etc/supervisor/supervisord.conf
